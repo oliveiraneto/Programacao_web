46064361ecf089dc9b0df48f34aba4c2a03fb6be
oliveiraneto <sebastiaonetosilva18@gmail.com>
2023-05-14 20:20:10 -0300

    modified:   "Atividade Pr\303\241tica Semana 11/PlanetModel.js" 	modified:   "Atividade Pr\303\241tica Semana 11/PlanetController.js"

--- Atividade Prática Semana 11/PlanetModel.js
+++ Atividade Prática Semana 11/PlanetModel.js
@@ -8,28 +8,33 @@ class PlanetModel {
       this.db = db;
     }
   
+    // Método para buscar todos os planetas
     async getAll() {
       const planets = await this.db.select().from('planets');
       return planets;
     }
   
+    // Método para buscar um planeta por ID
     async getById(id) {
       const planet = await this.db.select().from('planets').where({ id }).first();
       return planet;
     }
   
+    // Método para criar um novo planeta
     async create(planet) {
       const [id] = await this.db('planets').insert(planet);
       const newPlanet = await this.getById(id);
       return newPlanet;
     }
   
+    // Método para atualizar um planeta existente
     async update(planet) {
       await this.db('planets').where({ id: planet.id }).update(planet);
       const updatedPlanet = await this.getById(planet.id);
       return updatedPlanet;
     }
   
+    // Método para excluir um planeta existente
     async delete(planet) {
       await this.db('planets').where({ id: planet.id }).del();
     }
