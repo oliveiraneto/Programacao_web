5068670fd300852959eed7bab062776580b871ba
oliveiraneto <sebastiaonetosilva18@gmail.com>
2023-05-14 20:18:23 -0300

    modified:   "Atividade Pr\303\241tica Semana 10/node index.js"

--- Atividade Prática Semana 10/node index.js
+++ Atividade Prática Semana 10/node index.js
@@ -4,19 +4,33 @@
 //DATA 14/05/2023 -->
 
 const express = require('express');
+const knex = require('knex');
+const PlanetController = require('./controllers/PlanetController');
+const PlanetModel = require('./models/PlanetModel');
+
 const app = express();
 const port = 3000;
 
-const planetsRouter = require('./routes/planets');
-const informationsController = require('./controllers/informationsController');
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './database.sqlite',
+  },
+});
+
+const planetModel = new PlanetModel(db);
+const planetController = new PlanetController(planetModel);
 
 app.use(express.json());
 
-app.use('/api/planets', planetsRouter);
-
-// Endpoint GET /api/informations - Recuperar informações relacionadas ao projeto
-app.get('/api/informations', informationsController.getInformations);
+app.get('/api/planets', planetController.getAllPlanets.bind(planetController));
+app.get('/api/planets/:id', planetController.getPlanetById.bind(planetController));
+app.post('/api/planets', planetController.createPlanet.bind(planetController));
+app.put('/api/planets/:id', planetController.updatePlanet.bind(planetController));
+app.delete('/api/planets/:id', planetController.deletePlanet.bind(planetController));
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}.`);
 });
+
+
